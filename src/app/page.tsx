8a0b06c8af0e1824457af91b167532ff636698a5
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-11-13 15:24:41 +0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ export default function MultiProductCatalog() {
     <div className="flex flex-col min-h-screen">
       <header className="bg-blue-500 text-white p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">TECH ZONE</h1>
+          <h1 className="hidden md:block text-2xl font-bold">TECH ZONE</h1>
           <div className="relative">
             <input
               type="text"
